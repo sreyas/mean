718756a586cb24c135b3a7b72c65b53b0c82ce60
Saranya <ressellak@bitbucket.org>
2017-11-29 16:05:31 +0530

    file updated

--- modules/events/client/controllers/admin/event.client.controller.js
+++ modules/events/client/controllers/admin/event.client.controller.js
@@ -3,8 +3,10 @@
 
   angular
     .module('events.admin')
+    .controller('EventCategoryAdminController', EventCategoryAdminController)
     .controller('EventAdminController', EventAdminController);
 
+  EventCategoryAdminController.$inject = ['$scope', '$state', '$window', 'eventcategoryResolve', 'Authentication', 'Notification'];
   EventAdminController.$inject = ['$scope', '$state', '$window', 'eventResolve', 'Authentication', 'Notification'];
 
   function EventAdminController($scope, $state, $window, event, Authentication, Notification) {
@@ -48,4 +50,47 @@
       }
     }
   }
+   function EventCategoryAdminController($scope, $state, $window, eventcategory, Authentication, Notification) {
+    var vm = this;
+
+    vm.eventcategory = eventcategory;
+    vm.authentication = Authentication;
+    vm.form = {};
+    vm.remove = remove;
+    vm.save = save;
+
+    // Remove existing Event
+    function remove() {
+      if ($window.confirm('Are you sure you want to delete?')) {
+        vm.eventcategory.$remove(function () {
+          $state.go('admin.events.list');
+          Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Event category deleted successfully!' });
+        });
+      }
+    }
+
+    // Save Event
+    function save(isValid) {
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.form.eventcategoryForm');
+        return false;
+      }
+
+      // Create a new event, or update the current instance
+      vm.eventcategory.createOrUpdate()
+        .then(successCallback)
+        .catch(errorCallback);
+
+      function successCallback(res) {
+        $state.go('admin.eventcategory.list'); // should we send the User to the list or the updated Event's view?
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Event category saved successfully!' });
+      }
+
+      function errorCallback(res) {
+        Notification.error({ message: res.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Event save error!' });
+      }
+    }
+  }
+  
+  
 }());
