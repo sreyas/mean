a367fea425027ea863fef80e2014529b329b58d0
Saranya <ressellak@bitbucket.org>
2017-11-29 17:04:40 +0530

    file updated

--- modules/events/server/controllers/events.server.controller.js
+++ modules/events/server/controllers/events.server.controller.js
@@ -6,6 +6,7 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Event = mongoose.model('Event'),
+  EventCategory = mongoose.model('EventCategory')
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
 
 /**
@@ -25,6 +26,8 @@ exports.create = function (req, res) {
     }
   });
 };
+
+
 
 /**
  * Show the current event
@@ -112,6 +115,111 @@ exports.eventByID = function (req, res, next, id) {
       });
     }
     req.event = event;
+    next();
+  });
+};
+
+/**
+ * Create an Event Category
+ */
+ exports.create = function (req, res) {
+  var eventcategory = new EventCategory(req.body);
+  eventcategory.user = req.user;
+
+  eventcategory.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(eventcategory);
+    }
+  });
+};
+/**
+ * Show the current event Category
+ */
+exports.read = function (req, res) {
+  // convert mongoose document to JSON
+  var eventcategory = req.eventcategory ? req.eventcategory.toJSON() : {};
+
+  // Add a custom field to the Event, for determining if the current User is the "owner".
+  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Event model.
+  eventcategory.isCurrentUserOwner = !!(req.user && eventcategory.user && eventcategory.user._id.toString() === req.user._id.toString());
+
+  res.json(eventcategory);
+};
+/**
+ * Update an Event Category
+ */
+exports.update = function (req, res) {
+  var eventcategory = req.eventcategory;
+
+  eventcategory.title = req.body.title;
+
+  eventcategory.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(eventcategory);
+    }
+  });
+};
+/**
+ * Delete an Event Category
+ */
+exports.delete = function (req, res) {
+  var eventcategory = req.eventcategory;
+
+  eventcategory.remove(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(eventcategory);
+    }
+  });
+  
+  /**
+ * List of Events Category
+ */
+exports.list = function (req, res) {
+  EventCategory.find().sort('-created').populate('user', 'displayName').exec(function (err, eventcategory) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(eventcategory);
+    }
+  });
+};
+};
+
+
+/**
+ * Event category middleware
+ */
+exports.eventcategoryByID = function (req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Event Category is invalid'
+    });
+  }
+
+  EventCategory.findById(id).populate('user', 'displayName').exec(function (err, eventcategory) {
+    if (err) {
+      return next(err);
+    } else if (!eventcategory) {
+      return res.status(404).send({
+        message: 'No event category with that identifier has been found'
+      });
+    }
+    req.eventcategory = eventcategory;
     next();
   });
 };
--- modules/events/server/routes/events.server.routes.js
+++ modules/events/server/routes/events.server.routes.js
@@ -5,6 +5,7 @@
  */
 var eventsPolicy = require('../policies/events.server.policy'),
   events = require('../controllers/events.server.controller');
+  eventcategory = require('../controllers/events.server.controller');
 
 module.exports = function (app) {
   // Events collection routes
@@ -13,23 +14,42 @@ module.exports = function (app) {
     .post(events.create);
 
   // Single event routes
-  app.route('/api/events/:eventcategoryId').all(eventsPolicy.isAllowed)
+  app.route('/api/events/:eventId').all(eventsPolicy.isAllowed)
     .get(events.read)
     .put(events.update)
     .delete(events.delete);
 
   // Finish by binding the event middleware
   app.param('eventId', events.eventByID);
-   app.route('/api/eventcategory').all(eventsPolicy.isAllowed)
-    .get(eventcategory.list)
-    .post(eventcategory.create);
+  
+   app.route('/api/events').all(eventsPolicy.isAllowed)
+    .get(events.list)
+    .post(events.create);
 
   // Single event routes
-  app.route('/api/eventcategory/:eventcategoryId').all(eventsPolicy.isAllowed)
+  app.route('/api/events/:eventId').all(eventsPolicy.isAllowed)
+    .get(events.read)
+    .put(events.update)
+    .delete(events.delete);
+
+  // Finish by binding the event middleware
+  app.param('eventId', events.eventByID);
+  
+  app.route('/api/eventcategory').all(eventsPolicy.isAllowed)
+    .get(eventcategory.list)
+    .post(eventcategory.create);
+    
+     app.route('/api/eventcategory/:eventcategoryId').all(eventsPolicy.isAllowed)
     .get(eventcategory.read)
     .put(eventcategory.update)
     .delete(eventcategory.delete);
-
-  // Finish by binding the event middleware
+      // Finish by binding the event middleware
   app.param('eventcategoryId', eventcategory.eventcategoryByID);
+  
+   app.route('/api/eventcategory').all(eventsPolicy.isAllowed)
+    .get(eventcategory.list)
+    .post(eventcategory.create);
+    
+    app.param('eventcategoryId', eventcategory.eventcategoryByID);
+
 };
