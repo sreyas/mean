62eaa146bb4eea28f5f56539a01e1874b25e12d8
Saranya <ressellak@bitbucket.org>
2017-11-29 11:09:21 +0530

    files changed

--- modules/theaters/server/models/theater.server.model.js
+++ modules/theaters/server/models/theater.server.model.js
@@ -4,10 +4,7 @@
  * Module dependencies
  */
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  path = require('path'),
-  config = require(path.resolve('./config/config')),
-  chalk = require('chalk');
+  Schema = mongoose.Schema; 
 
 /**
  * Theater Schema
@@ -21,7 +18,7 @@ var TheaterSchema = new Schema({
     type: String,
     default: '',
     trim: true,
-    required: 'Title cannot be blank'
+    required: 'Title cannot  blank'
   },
   content: {
     type: String,
@@ -34,105 +31,4 @@ var TheaterSchema = new Schema({
   }
 });
 
-TheaterSchema.statics.seed = seed;
-
 mongoose.model('Theater', TheaterSchema);
-
-/**
-* Seeds the User collection with document (Theater)
-* and provided options.
-*/
-function seed(doc, options) {
-  var Theater = mongoose.model('Theater');
-
-  return new Promise(function (resolve, reject) {
-
-    skipDocument()
-      .then(findAdminUser)
-      .then(add)
-      .then(function (response) {
-        return resolve(response);
-      })
-      .catch(function (err) {
-        return reject(err);
-      });
-
-    function findAdminUser(skip) {
-      var User = mongoose.model('User');
-
-      return new Promise(function (resolve, reject) {
-        if (skip) {
-          return resolve(true);
-        }
-
-        User
-          .findOne({
-            roles: { $in: ['admin'] }
-          })
-          .exec(function (err, admin) {
-            if (err) {
-              return reject(err);
-            }
-
-            doc.user = admin;
-
-            return resolve();
-          });
-      });
-    }
-
-    function skipDocument() {
-      return new Promise(function (resolve, reject) {
-        Theater
-          .findOne({
-            title: doc.title
-          })
-          .exec(function (err, existing) {
-            if (err) {
-              return reject(err);
-            }
-
-            if (!existing) {
-              return resolve(false);
-            }
-
-            if (existing && !options.overwrite) {
-              return resolve(true);
-            }
-
-            // Remove Theater (overwrite)
-
-            existing.remove(function (err) {
-              if (err) {
-                return reject(err);
-              }
-
-              return resolve(false);
-            });
-          });
-      });
-    }
-
-    function add(skip) {
-      return new Promise(function (resolve, reject) {
-        if (skip) {
-          return resolve({
-            message: chalk.yellow('Database Seeding: Theater\t' + doc.title + ' skipped')
-          });
-        }
-
-        var theater = new Theater(doc);
-
-        theater.save(function (err) {
-          if (err) {
-            return reject(err);
-          }
-
-          return resolve({
-            message: 'Database Seeding: Theater\t' + theater.title + ' added'
-          });
-        });
-      });
-    }
-  });
-}
