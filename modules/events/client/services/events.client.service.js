b71d8f9d1d23a13bdd1e3a9677bac2df61bfa7e8
Saranya <ressellak@bitbucket.org>
2017-11-30 16:39:09 +0530

    File updated

--- modules/events/client/services/events.client.service.js
+++ modules/events/client/services/events.client.service.js
@@ -1,27 +1,99 @@
 (function () {
   'use strict';
- var myApp = angular.module('events.admin', ['ngResource']);
- myApp.factory('EventsService', ['$resource', function ($resource) {
-
-return $resource('/api/events/:eventId', {
-      eventId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
-   
-}]);
-myApp.factory('EventCategoryService', ['$resource', function ($resource) {
+ 
+      angular.module('events.services')
+      .factory('EventsService', EventsService)
+      .factory('EventCategoryService', EventCategoryService);
 
 
-    return $resource('/api/eventcategory/:eventcategoryId', {
+  EventsService.$inject = ['$resource', '$log'];
+  EventCategoryService.$inject = ['$resource', '$log'];
+  
+    function EventCategoryService($resource, $log) {
+    var EventCategory = $resource('/api/eventcategory/:eventcategoryId', {
       eventcategoryId: '@_id'
     }, {
       update: {
         method: 'PUT'
       }
     });
-}]);
 
+    angular.extend(EventCategory.prototype, {
+      createOrUpdate: function () {
+        var eventcategory = this;
+        return createOrUpdate(eventcategory);
+      }
+    });
+
+   
+
+    function createOrUpdate(eventcategory) {
+      if (eventcategory._id) {
+        return eventcategory.$update(onSuccess, onError);
+      } else {
+        return eventcategory.$save(onSuccess, onError);
+      }
+
+      // Handle successful response
+      function onSuccess(eventcategory) {
+        // Any required internal processing from inside the service, goes here.
+      }
+
+      // Handle error response
+      function onError(errorResponse) {
+        var error = errorResponse.data;
+        // Handle error internally
+        handleError(error);
+      }
+    }
+
+    function handleError(error) {
+      // Log error
+      $log.error(error);
+    }
+    return EventCategory;
+  }
+  function EventsService($resource, $log) {
+    var Event = $resource('/api/events/:eventId', {
+      eventId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+
+    angular.extend(Event.prototype, {
+      createOrUpdate: function () {
+        var event = this;
+        return createOrUpdate(event);
+      }
+    });
+
+    return Event;
+
+    function createOrUpdate(event) {
+      if (event._id) {
+        return event.$update(onSuccess, onError);
+      } else {
+        return event.$save(onSuccess, onError);
+      }
+
+      // Handle successful response
+      function onSuccess(event) {
+        // Any required internal processing from inside the service, goes here.
+      }
+
+      // Handle error response
+      function onError(errorResponse) {
+        var error = errorResponse.data;
+        // Handle error internally
+        handleError(error);
+      }
+    }
+
+    function handleError(error) {
+      // Log error
+      $log.error(error);
+    }
+  }
 }());
