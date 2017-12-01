ee6befc8cf657bc7e084b97f8af9cbd26dab1997
Saranya <ressellak@bitbucket.org>
2017-12-01 13:48:34 +0530

    remove bootstrap css and js files

--- config/assets/default.js
+++ config/assets/default.js
@@ -7,16 +7,18 @@ module.exports = {
     lib: {
       css: [
         // bower:css
-        'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
+       // 'public/lib/bootstrap/dist/css/bootstrap.css',
+        //'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+       // 'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
+         'public/lib/angular-material/angular-material.css'
         // endbower
       ],
       js: [
         // bower:js
         'public/lib/angular/angular.js',
         'public/lib/angular-animate/angular-animate.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+       // 'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-material/angular-material.min.js',
         'public/lib/ng-file-upload/ng-file-upload.js',
         'public/lib/angular-messages/angular-messages.js',
         'public/lib/angular-mocks/angular-mocks.js',
