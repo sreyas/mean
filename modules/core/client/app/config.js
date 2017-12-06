542c17a1aefea405faccbff9e5e6a549d00d3e3e
Saranya <ressellak@bitbucket.org>
2017-12-06 12:18:47 +0530

    changing bootstrap to materailze for all pagesd

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -7,7 +7,7 @@
     applicationEnvironment: window.env,
     applicationModuleName: applicationModuleName,
     //applicationModuleVendorDependencies: ['ngResource', 'ngMessages', 'ui.router', 'ngFileUpload', 'ui-notification'],
-    applicationModuleVendorDependencies:['ngResource', 'ngMessages', 'ui.router', 'ngFileUpload', 'ui-notification','ngMaterial']
+    applicationModuleVendorDependencies:['ngResource', 'ngMessages', 'ui.router', 'ngFileUpload', 'ui-notification','ngMaterial'],
     registerModule: registerModule
   };
 
