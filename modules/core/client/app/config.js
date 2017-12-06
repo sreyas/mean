874034df1fb5560b965bef89873d8e146c20a9c3
Saranya <ressellak@bitbucket.org>
2017-12-06 18:48:15 +0530

    changing bootstrap to materailze for all pagesd

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -15,7 +15,7 @@
   // Add a new vertical module
   function registerModule(moduleName, dependencies) {
     // Create angular module
-    angular.module(moduleName, dependencies || []);
+    angular.module(moduleName, dependencies);
 
     // Add the module to the AngularJS configuration file
     angular.module(applicationModuleName).requires.push(moduleName);   
