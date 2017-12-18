7e3d3e499fa462fa647c1586a0b1767a2130b740
Ciril Sreedhar <ciril.sreedhar@sreyas.com>
2017-12-18 11:37:19 +0530

    files added

---
+++ modules/events/client/config/events-admin.client.config.js
@@ -0,0 +1,21 @@
+﻿(function () {
+  'use strict';
+
+  // Configuring the Events Admin module
+  angular
+    .module('events.admin')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(Menus) {
+    Menus.addSubMenuItem('topbar', 'admin', {
+      title: 'Manage Events',
+      state: 'admin.events.list'
+    });
+     Menus.addSubMenuItem('topbar', 'admin', {
+      title: 'Manage Event Categories',
+      state: 'admin.eventcategory.list'
+    });
+  }
+}());
---
+++ modules/events/client/controllers/admin/list-events.client.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('events.admin')
+    .controller('EventsAdminListController', EventsAdminListController);
+
+  EventsAdminListController.$inject = ['EventsService'];
+
+  function EventsAdminListController(EventsService) {
+    var vm = this;
+    vm.events = EventsService.query();
+  }
+}());
